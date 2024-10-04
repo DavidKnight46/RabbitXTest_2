9bbdc941a6929f9d747f5bfddcb979caeb816da0
David Knight <disco.david.85@gmail.com>
2024-10-04 17:16:24 +0800

    Files added.

---
+++ RabbitBETest/src/main/java/org/rabbitx/rabbitbetest/RabbitBeTestApplication.java
@@ -0,0 +1,13 @@
+package org.rabbitx.rabbitbetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitBeTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitBeTestApplication.class, args);
+    }
+
+}
